ae6f53660a92192ed65d7fb055994697333be7cc
Luce, Thom <luce@ohio.edu>
2020-03-26 11:30:27 -0400

    Push changes to .gitignore

---
+++ IACIS2020/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IACIS2020.MvcApplication" Language="C#" %>
---
+++ IACIS2020/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IACIS2020.Startup))]
+namespace IACIS2020
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
